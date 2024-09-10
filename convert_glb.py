889816aceaeab4047cfb79fd83642ac777cb73de
Ethan Chien <liangtie.qian@gmail.com>
2024-09-10 23:35:55 +0800

    Update ci cmd

--- convert_glb.py
+++ convert_glb.py
@@ -22,7 +22,7 @@ def export_glb(root_sch_file_name):
     first_cmd = ["docker", "run", "--rm",
                  "-v", f"{kicad_project_dir}:{mounted_prj_path}",
                  KICAD_FULL_IMAGE_ID, "kicad-cli", "pcb",
-                 "export", "glb", "--subst-models", "--include-tracks",
+                 "export", "glb", "--subst-models", "--include-tracks","--include-pads" , "--include-zones", "--include-silkscreen",
                  mouted_pcb_fp, "-o",
                  docker_output_fn
                  ]
